928fa7f6d3fb8421ba0f0259ce2d1e9081854122
Ravindra Marella <mv.ravindra007@gmail.com>
2021-06-23 21:06:34 +0530

    Show changes when downloading metadata

--- scripts/metadata.js
+++ scripts/metadata.js
@@ -9,6 +9,7 @@ export const downloadVersions = async (dir) => {
   const file = path.resolve(dir, 'versions.json');
   console.log(`Downloading ${file}`);
   const versions = await getVersions();
+  await logChanges(file, versions);
   await fs.writeFile(file, JSON.stringify(versions, null, 2));
   console.log('Done');
 };
@@ -27,3 +28,53 @@ const getMetadata = async () => {
   data = data.substring(data.indexOf('\n')); // remove first line
   return JSON.parse(data);
 };
+
+const logChanges = async (file, latest) => {
+  let existing;
+  try {
+    existing = JSON.parse(await fs.readFile(file));
+  } catch (e) {
+    if (e.code !== 'ENOENT') {
+      console.error(e.message);
+    }
+    return;
+  }
+  const { removed, updated, added } = getChanges(existing, latest);
+  if (removed.length === 0 && updated.length === 0 && added.length === 0) {
+    console.log('No changes');
+    return;
+  }
+  console.log('Changes:');
+  if (removed.length !== 0) {
+    console.log(`- Remove ${removed.length} icons: ${names(removed)}`);
+  }
+  if (updated.length !== 0) {
+    console.log(`- Update ${updated.length} icons`);
+  }
+  if (added.length !== 0) {
+    console.log(`- Add ${added.length} icons: ${names(added)}`);
+  }
+};
+
+const getChanges = (existing, latest) => {
+  existing = sortMap(existing);
+  latest = sortMap(latest);
+  const removed = [];
+  const updated = [];
+  const added = [];
+  for (const name of Object.keys(existing)) {
+    if (!latest[name]) {
+      removed.push(name);
+    } else if (latest[name] !== existing[name]) {
+      updated.push(name);
+    }
+  }
+  for (const name of Object.keys(latest)) {
+    if (!existing[name]) {
+      added.push(name);
+    }
+  }
+  return { removed, updated, added };
+};
+
+const names = (names) => names.map((name) => '`' + name + '`').join(', ');
